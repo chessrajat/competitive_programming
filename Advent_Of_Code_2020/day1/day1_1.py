3793d1ec96b75216e4753bdb40b266d5724cda06
Rajat Tyagi <raat66rat@gmail.com>
2020-12-11 10:41:30 +0530

    advent of code 2020 day1 completed

--- Advent_Of_Code_2020/day1/day1_1.py
+++ Advent_Of_Code_2020/day1/day1_1.py
@@ -14,19 +14,26 @@
 # 675
 # 1456
 
-def find_numbers_of_sum(num_list, number):
+def find_2_numbers_of_sum(num_list, number):
     for i in num_list:
         check_num = number - i
-        for j in num_list:
-            if check_num == j:
-                return i*j
+        if check_num in num_list:
+                return i*check_num
 
+def find_3_numbers_of_sum(num_list, number):
+    for i in num_list:
+        for j in num_list:
+            check_num = number - i - j
+            if check_num in num_list:
+                return i * j * check_num
 
 
 if __name__ == "__main__":
+    sample_list = [1721, 979, 366, 299, 675, 1456]
     with open("input1_1.txt","r") as f:
         num_list = f.readlines()
 
     num_list = [int(i.strip("\n")) for i in num_list]
-    ans = find_numbers_of_sum(num_list,2020)
+    # ans = find_2_numbers_of_sum(sample_list,2020)
+    ans = find_3_numbers_of_sum(num_list, 2020)
     print(ans)
